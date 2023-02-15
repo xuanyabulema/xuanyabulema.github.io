aea45ae838647ad870d6056caa765a9dc477a25a
xuanyabulema <xuanyabulema@qq.com>
2023-02-15 22:27:38 +0800

    Update hexo_d_and_restart_docker_nginx.sh

--- hexo_d_and_restart_docker_nginx.sh
+++ hexo_d_and_restart_docker_nginx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "将要执行：git pull"
+echo "将要执行：git pull"
+echo "将要执行：git pull"
+sleep 1
+git pull
+
 echo "将要执行：hexo clean && hexo g && hexo d"
 echo "将要执行：hexo clean && hexo g && hexo d"
 echo "将要执行：hexo clean && hexo g && hexo d"
